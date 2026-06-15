a30758d02d2544593f67a5c41d46f630b9f8edaa
vurado51171-del <vurado51171@gmail.com>
2026-06-15 16:54:29 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+  const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+// Указываем серверу отдавать файл index.html, когда кто-то заходит на http://localhost:3000
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Логика работы с WebSocket-соединениями
+io.on('connection', (socket) => {
+    console.log('Пользователь подключился');
+
+    // Когда сервер получает событие 'chat message' от кого-то
+    socket.on('chat message', (msg) => {
+        // Он пересылает это сообщение ВСЕМ подключенным пользователям
+        io.emit('chat message', msg);
+    });
+
+    // Когда пользователь закрывает вкладку
+    socket.on('disconnect', () => {
+        console.log('Пользователь отключился');
+    });
+});
+
+// Стартуем сервер
+const PORT = 3000;
+http.listen(PORT, () => {
+    console.log(`Сервер запущен! Открой в браузере: http://localhost:${PORT}`);
+});
